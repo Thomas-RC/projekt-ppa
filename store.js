e519fbe1d42dc68771a16527f30fbdf6bd4b664a
Thomas-RC <tomaszrosik@hotmail.com>
2020-03-15 23:29:57 +0000

    Dodano funkcję tworzenia zadań

--- store.js
+++ store.js
@@ -1,6 +1,7 @@
 const storage = require('azure-storage')
 const service = storage.createTableService()
 const table = 'tasks'
+const uuid = require('uuid')
 
 const init = async () => (
   new Promise((resolve, reject) => {
@@ -10,6 +11,22 @@ const init = async () => (
   })
 )
 
+const createTask = async (title) => (
+  new Promise((resolve, reject) => {
+    const generator = storage.TableUtilities.entityGenerator
+    const task = {
+      PartitionKey: generator.String('task'),
+      RowKey: generator.String(uuid.v4()),
+      title
+    }
+
+    service.insertEntity(table, task, (error, result, response) => {
+      !error ? resolve() : reject()
+    })
+  })
+)
+
 module.exports = {
-  init
+  init,
+  createTask
 }
