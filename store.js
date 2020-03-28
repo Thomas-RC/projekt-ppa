ff2f8fed02c62f1d3ecbb6e1092c37eba86919b5
Thomas-RC <tomaszrosik@hotmail.com>
2020-03-28 13:32:08 +0000

    Dodano możliwość zmiany statusu zadania

--- store.js
+++ store.js
@@ -12,36 +12,55 @@ const init = async () => (
 )
 
 const createTask = async (title) => (
-  new Promise((resolve, reject) => {
-    const generator = storage.TableUtilities.entityGenerator
-    const task = {
-      PartitionKey: generator.String('task'),
-      RowKey: generator.String(uuid.v4()),
-      title
-    }
+    new Promise((resolve, reject) => {
+        const generator = storage.TableUtilities.entityGenerator
+        const task = {
+            PartitionKey: generator.String('task'),
+            RowKey: generator.String(uuid.v4()),
+            title,
+            status: 'open'
+        }
 
-    service.insertEntity(table, task, (error, result, response) => {
-      !error ? resolve() : reject()
+        service.insertEntity(table, task, (error, result, response) => {
+            !error ? resolve() : reject()
+        })
     })
-  })
 )
 
 const listTasks = async () => (
     new Promise((resolve, reject) => {
         const query = new storage.TableQuery()
-            .select(['title'])
+            .select(['RowKey', 'title', 'status'])
             .where('PartitionKey eq ?', 'task')
 
         service.queryEntities(table, query, null, (error, result, response) => {
             !error ? resolve(result.entries.map((entry) => ({
-                title: entry.title._
+                id: entry.RowKey._,
+                title: entry.title._,
+                status: entry.status._
             }))) : reject()
         })
     })
 )
 
+const updateTaskStatus = async (id, status) => (
+    new Promise((resolve, reject) => {
+        const generator = storage.TableUtilities.entityGenerator
+        const task = {
+            PartitionKey: generator.String('task'),
+            RowKey: generator.String(id),
+            status
+        }
+
+        service.mergeEntity(table, task, (error, result, response) => {
+            !error ? resolve() : reject()
+        })
+    })
+)
+
 module.exports = {
-  init,
-  createTask,
-  listTasks
+    init,
+    createTask,
+    listTasks,
+    updateTaskStatus
 }
